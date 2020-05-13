64ee604f0f5b49b80c3a42ec6c413fc729e74857
FRANCESCA PONZETTA s276535 <s276535@studenti.polito.it>
2020-05-13 18:04:16 +0200

    added userConverter

--- src/main/java/it/polito/ezgas/converter/UserConverter.java
+++ src/main/java/it/polito/ezgas/converter/UserConverter.java
@@ -1,4 +1,36 @@
 package it.polito.ezgas.converter;
 
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+
 public class UserConverter {
+	
+	
+	
+	
+	UserDto convertEntityToEntity(User us) {
+		UserDto Udto=new UserDto();
+		Udto.setUserId(us.getUserId());
+		Udto.setEmail(us.getEmail());
+		Udto.setPassword(us.getPassword());
+		Udto.setAdmin(us.getAdmin());
+		Udto.setReputation(us.getReputation());
+		Udto.setUserName(us.getUserName());
+		
+		return Udto;
+	}
+	
+	User convertDtoToEntity(UserDto us) {
+		User Uentity=new User();
+		Uentity.setUserId(us.getUserId());
+		Uentity.setEmail(us.getEmail());
+		Uentity.setPassword(us.getPassword());
+		Uentity.setAdmin(us.getAdmin());
+		Uentity.setReputation(us.getReputation());
+		Uentity.setUserName(us.getUserName());
+		
+		return Uentity;
+	}
+	
+	
 }
