efcfcda6a72a73fd7db00652b4dea120c8d2652f
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-13 18:22:27 +0200

    add findByUserName method to search the DB with a username String

--- src/main/java/it/polito/ezgas/Repository/UserRepository.java
+++ src/main/java/it/polito/ezgas/Repository/UserRepository.java
@@ -4,4 +4,5 @@ import it.polito.ezgas.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserName(String username);
 }
