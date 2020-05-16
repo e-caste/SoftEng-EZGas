286fd5dacc5c82482ac6c57705bfdab906b15da2
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-16 15:13:51 +0200

    add testSaveUser method to UserServiceimplTests class. ref #24

--- src/test/java/it/polito/ezgas/UserServiceimplTests.java
+++ src/test/java/it/polito/ezgas/UserServiceimplTests.java
@@ -4,7 +4,6 @@ import exception.InvalidUserException;
 import it.polito.ezgas.converter.UserConverter;
 import it.polito.ezgas.dto.UserDto;
 import it.polito.ezgas.entity.User;
-import it.polito.ezgas.impl.UserServiceimpl;
 
 import static org.junit.Assert.*;
 
@@ -14,8 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 // IMPORTANT NOTE: these tests can't run if the website is up and running,
@@ -30,36 +27,44 @@ public class UserServiceimplTests {
     @Autowired
     private UserService userService;
 
-    Integer existingUserId, nonExistingUserId;
-    Boolean existingUserAdmin, nonExistingUserAdmin;
-    private User existingUser, nonExistingUser;
-    private UserDto existingUserDto, nonExistingUserDto;
+    Integer existingAdminUserId, existingUserId, nonExistingUserId;
+    Boolean existingAdminUserAdmin, existingUserAdmin, nonExistingUserAdmin;
+    private User existingAdminUser, existingUser, nonExistingUser;
+    private UserDto existingAdminUserDto, existingUserDto, nonExistingUserDto;
 
     @Before  // run only once
     public void setUp() {
 
+        // admin user with existing id in the database
+        this.existingAdminUser = new User("admin", "admin", "admin@ezgas.com", 5);
+        this.existingAdminUserId = 2;
+        this.existingAdminUserAdmin = true;
+        this.existingAdminUser.setUserId(this.existingAdminUserId);
+        this.existingAdminUser.setAdmin(this.existingAdminUserAdmin);
+        this.existingAdminUserDto = UserConverter.convertEntityToDto(this.existingAdminUser);
+
         // user with existing id in the database
-        this.existingUser = new User("admin", "admin", "admin@ezgas.com", 5);
-        this.existingUserId = 2;
-        this.existingUserAdmin = true;
-        existingUser.setUserId(this.existingUserId);
-        existingUser.setAdmin(this.existingUserAdmin);
-        this.existingUserDto = UserConverter.convertEntityToDto(existingUser);
+        this.existingUser = new User("asd", "asd", "asd@asd.asd", 0);
+        this.existingUserId = 1;
+        this.existingUserAdmin = false;
+        this.existingUser.setUserId(this.existingUserId);
+        this.existingUser.setAdmin(this.existingUserAdmin);
+        this.existingUserDto = UserConverter.convertEntityToDto(this.existingUser);
 
         // user with non-existing id in the database
         this.nonExistingUser = new User("test", "test", "test", 0);
         this.nonExistingUserId = 1000;
         this.nonExistingUserAdmin = false;
-        nonExistingUser.setUserId(this.nonExistingUserId);
-        nonExistingUser.setAdmin(this.nonExistingUserAdmin);
-        this.nonExistingUserDto = UserConverter.convertEntityToDto(nonExistingUser);
+        this.nonExistingUser.setUserId(this.nonExistingUserId);
+        this.nonExistingUser.setAdmin(this.nonExistingUserAdmin);
+        this.nonExistingUserDto = UserConverter.convertEntityToDto(this.nonExistingUser);
 
 //        this.userService.getAllUsers().forEach(System.out::println);
     }
 
     @Test
     public void testGetUserById() throws InvalidUserException {
-        assertTrue(this.existingUserDto.equals(this.userService.getUserById(this.existingUserId)));
+        assertTrue(this.existingAdminUserDto.equals(this.userService.getUserById(this.existingAdminUserId)));
         // assertThrows() - not available in this version of JUnit4
         try {
             this.userService.getUserById(this.nonExistingUserId);
@@ -67,5 +72,34 @@ public class UserServiceimplTests {
         } catch (InvalidUserException e) {
             assertEquals(e.getMessage(), "User not found");
         }
+    }
+
+    @Test
+    public void testSaveUser() {
+        // local copy of class variables
+        User nonExistingUser = this.nonExistingUser;
+        User existingUser = this.existingUser;
+        UserDto nonExistingUserDto, existingUserDto;
+
+        // user does not exist in database
+        // and is admin -> reject (only one admin allowed)
+        nonExistingUser.setAdmin(true);
+        nonExistingUser.setUserId(null);
+        nonExistingUserDto = UserConverter.convertEntityToDto(nonExistingUser);
+        assertNull(this.userService.saveUser(nonExistingUserDto));
+
+        // and is valid -> save new
+        assertTrue(nonExistingUserDto.equalsIgnoreUserId(this.userService.saveUser(nonExistingUserDto)));
+
+        // user exists in database
+        // user id exists and email is same -> update user fields in database
+        existingUser.setPassword("newPassword");
+        existingUserDto = UserConverter.convertEntityToDto(existingUser);
+        assertTrue(existingUserDto.equals(this.userService.saveUser(existingUserDto)));
+
+        // user id exists but updates email -> reject (email must be invariable)
+        existingUser.setEmail("qwe@qwe.qwe");
+        existingUserDto = UserConverter.convertEntityToDto(existingUser);
+        assertNull(this.userService.saveUser(existingUserDto));
     }
 }
