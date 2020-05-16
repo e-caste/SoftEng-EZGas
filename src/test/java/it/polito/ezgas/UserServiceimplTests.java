821a9a18766bb50103dbc692758833173528178e
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-16 13:10:46 +0200

    create test file. add testGetUserById method of UserServiceimplTests class. ref #24

--- src/test/java/it/polito/ezgas/UserServiceimplTests.java
+++ src/test/java/it/polito/ezgas/UserServiceimplTests.java
@@ -1,4 +1,69 @@
 package it.polito.ezgas;
 
-public class UseServiceimplTests {
+import exception.InvalidUserException;
+import it.polito.ezgas.converter.UserConverter;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+import it.polito.ezgas.impl.UserServiceimpl;
+
+import static org.junit.Assert.*;
+
+import it.polito.ezgas.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@Configuration
+//@ComponentScan("it.polito.ezgas.repository")
+public class UserServiceimplTests {
+
+    // using UserService instead of UserServiceimpl since it's the interface
+    @Autowired
+    private UserService userService;
+
+    Integer existingUserId, nonExistingUserId;
+    Boolean existingUserAdmin, nonExistingUserAdmin;
+    private User existingUser, nonExistingUser;
+    private UserDto existingUserDto, nonExistingUserDto;
+
+    @Before  // run only once
+    public void setUp() {
+
+        // user with existing id in the database
+        this.existingUser = new User("admin", "admin", "admin@ezgas.com", 5);
+        this.existingUserId = 2;
+        this.existingUserAdmin = true;
+        existingUser.setUserId(this.existingUserId);
+        existingUser.setAdmin(this.existingUserAdmin);
+        this.existingUserDto = UserConverter.convertEntityToDto(existingUser);
+
+        // user with non-existing id in the database
+        this.nonExistingUser = new User("test", "test", "test", 0);
+        this.nonExistingUserId = 1000;
+        this.nonExistingUserAdmin = false;
+        nonExistingUser.setUserId(this.nonExistingUserId);
+        nonExistingUser.setAdmin(this.nonExistingUserAdmin);
+        this.nonExistingUserDto = UserConverter.convertEntityToDto(nonExistingUser);
+
+        this.userService.getAllUsers().forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetUserById() throws InvalidUserException {
+        assertTrue(this.existingUserDto.equals(this.userService.getUserById(this.existingUserId)));
+        // assertThrows() - not available in this version of JUnit4
+        try {
+            this.userService.getUserById(this.nonExistingUserId);
+            fail("Expected InvalidUserException for userId " + this.nonExistingUserId);
+        } catch (InvalidUserException e) {
+            assertEquals(e.getMessage(), "User not found");
+        }
+    }
 }
