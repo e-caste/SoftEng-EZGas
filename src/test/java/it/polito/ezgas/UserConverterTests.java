956bf2a387cba92e6e84237fa3c20776b652f6d4
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-17 11:14:42 +0200

    add constants and setUp method to UserConverterTests class. ref #27

--- src/test/java/it/polito/ezgas/UserConverterTests.java
+++ src/test/java/it/polito/ezgas/UserConverterTests.java
@@ -1,5 +1,9 @@
 package it.polito.ezgas;
 
+import it.polito.ezgas.dto.LoginDto;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +12,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserConverterTests {
+
+    User user;
+    UserDto userDto;
+    LoginDto loginDto;
+
+    // constants to initialize variables in setUp method
+    String username = "username", password = "password", email = "test@test.test", token = "token";
+    Integer reputation = 0, userId = 10;
+    Boolean admin = false;
+
+    @Before
+    public void setUp() {
+        user = new User(username, password, email, reputation);
+        user.setAdmin(admin);
+        user.setUserId(userId);
+
+        userDto = new UserDto(userId, username, password, email, reputation, admin);
+
+        loginDto = new LoginDto(userId, username, token, email, reputation);
+        loginDto.setAdmin(admin);
+    }
 
     @Test
     public void testConvertEntityToDto() {
