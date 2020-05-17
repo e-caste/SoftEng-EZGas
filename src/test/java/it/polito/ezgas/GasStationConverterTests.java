cd28ceeb6b243d1bbd464da52a39c39103fb6aca
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-17 11:54:44 +0200

    add setUp method to GasStationConverterTests class. ref #28

--- src/test/java/it/polito/ezgas/GasStationConverterTests.java
+++ src/test/java/it/polito/ezgas/GasStationConverterTests.java
@@ -19,26 +19,71 @@ public class GasStationConverterTests {
     GasStationDto gasStationDto;
 
     // constants to initialize variables in setUp method
-    Integer gasStationId = 10, reportUser = 42;
+    Integer gasStationId = 1,
+            reportUser = 2;  // userId of User who submitted the last priceReport
     String gasStationName = "name",
-            getGasStationAddress = "Corso Duca degli Abruzzi 24",
-            carSharing = "Enjoy",
-            reportTimestamp = "1589707858";
+           gasStationAddress = "Corso Duca degli Abruzzi 24",
+           carSharing = "Enjoy",
+           reportTimestamp = "1589707858";
     boolean hasDiesel = true,
             hasSuper = false,
             hasSuperPlus = true,
             hasGas = false,
             hasMethane = true;
     double lat = 45.06248474121094,
-            lon = 7.662814140319824,
-            reportDependability = 3.33,
-            dieselPrice = 1.29,
-            superPrice = 1.12,
-            superPlusPrice = 1.18,
-            gasPrice = 1.43,
-            methanePrice = 1.12;
-    User user = new User("username", "password", "em@i.l", 0);
+           lon = 7.662814140319824,
+           reportDependability = 3.33,
+           dieselPrice = 1.29,
+           superPrice = 1.12,
+           superPlusPrice = 1.18,
+           gasPrice = 1.43,
+           methanePrice = 1.12;
 
+    @Before
+    public void setUp() {
+        gasStation = new GasStation(
+                gasStationName,
+                gasStationAddress,
+                hasDiesel,
+                hasSuper,
+                hasSuperPlus,
+                hasGas,
+                hasMethane,
+                carSharing,
+                lat,
+                lon,
+                dieselPrice,
+                superPrice,
+                superPlusPrice,
+                gasPrice,
+                methanePrice,
+                reportUser,
+                reportTimestamp,
+                reportDependability
+        );
+        gasStation.setGasStationId(gasStationId);
 
+        gasStationDto = new GasStationDto(
+                gasStationId,
+                gasStationName,
+                gasStationAddress,
+                hasDiesel,
+                hasSuper,
+                hasSuperPlus,
+                hasGas,
+                hasMethane,
+                carSharing,
+                lat,
+                lon,
+                dieselPrice,
+                superPrice,
+                superPlusPrice,
+                gasPrice,
+                methanePrice,
+                reportUser,
+                reportTimestamp,
+                reportDependability
+        );
+    }
 
 }
