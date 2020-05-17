7f29d4a54fbd07bcfcd28c95771ad6ce239a5c93
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-17 10:35:07 +0200

    add UserConverterTests class with empty methods. ref #27

---
+++ src/test/java/it/polito/ezgas/UserConverterTests.java
@@ -0,0 +1,26 @@
+package it.polito.ezgas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserConverterTests {
+
+    @Test
+    public void testConvertEntityToDto() {
+
+    }
+
+    @Test
+    public void testConvertDtoToEntity() {
+
+    }
+
+    @Test
+    public void testConvertEntityToLoginDto() {
+
+    }
+}
