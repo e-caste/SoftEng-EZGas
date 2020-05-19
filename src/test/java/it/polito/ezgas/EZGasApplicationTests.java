b87b57a68f8dece498b6ca9950c1665106e613fa
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-19 16:05:08 +0200

    use EZGasApplicationTests class to run all tests as a single Suite

--- src/test/java/it/polito/ezgas/EZGasApplicationTests.java
+++ src/test/java/it/polito/ezgas/EZGasApplicationTests.java
@@ -2,10 +2,19 @@ package it.polito.ezgas;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		GasStationConverterTests.class,
+		GasStationServiceimplTests.class,
+		GetterSetterTests.class,
+		UserConverterTests.class,
+		UserServiceimplTests.class
+})
 @SpringBootTest
 public class EZGasApplicationTests {
 
