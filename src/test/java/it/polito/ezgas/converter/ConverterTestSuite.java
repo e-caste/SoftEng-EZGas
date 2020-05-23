9051178e4ed66ed7388dcb3faa515b84d4b6288c
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-23 16:53:43 +0200

    add content to ConverterTestSuite and ServiceTestSuite classes. ref #33

--- src/test/java/it/polito/ezgas/converter/ConverterTestSuite.java
+++ src/test/java/it/polito/ezgas/converter/ConverterTestSuite.java
@@ -1,4 +1,13 @@
 package it.polito.ezgas.converter;
 
-public class ConverterTestSuite {
-}
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        GasStationConverterTests.class,
+        UserConverterTests.class
+})
+@SpringBootTest
+public class ConverterTestSuite {}
--- src/test/java/it/polito/ezgas/service/ServiceTestSuite.java
+++ src/test/java/it/polito/ezgas/service/ServiceTestSuite.java
@@ -1,4 +1,13 @@
 package it.polito.ezgas.service;
 
-public class ServiceTestSuite {
-}
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        GasStationServiceimplTests.class,
+        UserServiceimplTests.class
+})
+@SpringBootTest
+public class ServiceTestSuite {}
