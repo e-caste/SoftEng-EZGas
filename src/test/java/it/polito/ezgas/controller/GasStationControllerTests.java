b8b9a70c1428507267c96c9ba71014356b6b0350
FRANCESCA PONZETTA s276535 <s276535@studenti.polito.it>
2020-05-24 12:37:43 +0200

    added class GasStationControllerTests

---
+++ src/test/java/it/polito/ezgas/controller/GasStationControllerTests.java
@@ -0,0 +1,52 @@
+package it.polito.ezgas.controller;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = GasStationController)
+public class GasStationControllerTests {
+
+
+
+    @Test
+    public void test_getGasStationById(){
+
+    }
+
+    @Test
+    public void test_getAllGasStations(){
+
+    }
+
+    @Test
+    public void test_deleteGasStation(){
+
+    }
+
+    @Test
+    public void test_getGasStationsByGasolineType(){
+
+    }
+
+    @Test
+    public void test_getGasStationsByProximity(){
+
+    }
+
+    @Test
+    public void test_getGasStationsWithCoordinates(){
+
+    }
+
+    @Test
+    public void test_setGasStationReport(){
+
+    }
+
+
+
+}
