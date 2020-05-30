6b5bf4b924561d7801399a2de03acc72e5289888
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-30 09:37:47 +0200

    add utility methods getJsonFromResponse and getMapper to TestController class. ref #40 ref #41

--- src/test/java/it/polito/ezgas/controllertests/TestController.java
+++ src/test/java/it/polito/ezgas/controllertests/TestController.java
@@ -2,10 +2,13 @@ package it.polito.ezgas.controllertests;
 
 import static it.polito.ezgas.utils.Constants.*;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -17,6 +20,14 @@ public class TestController {
     static String apiPrefixGasStation = "gasstation/";
 
     static Integer existingAdminUserId = 1, existingUserId = 2, nonExistingUserId = 1000;
+
+    private String getJsonFromResponse(HttpResponse response) throws IOException {
+        return EntityUtils.toString(response.getEntity());
+    }
+
+    private ObjectMapper getMapper() {
+        return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     // UserController tests
     @Test
@@ -37,6 +48,10 @@ public class TestController {
         assert response.getStatusLine().getStatusCode() == 404;
     }
 
+    @Test
+    public void testGetAllUsers() {
+
+    }
 
     // GasStationController tests
 }
