2c737b9792a0f5c10a467f462ab4c2d352098f4c
Augusto Maria Guerriero <s278018@studenti.polito.it>
2020-06-02 23:53:40 +0200

    Split TestGui.py in TestUseCase*.py. Deleted TestGui.py. Ref #38

---
+++ src/test/java/it/polito/ezgas/GUITests/TestUseCase10.py
@@ -0,0 +1,11 @@
+''' Test Use Case 10 '''
+#Evaluate price
+type("1591045945025.png", "Corso Duca degli Abruzzi Turin Piemont Italy")
+click("1591046398385.png")
+click("1591046172918.png")
+click(Pattern("1591047242981.png").targetOffset(-37,6))
+click("1591046267171.png")
+sleep(3)
+
+click(Pattern("1591047715063.png").targetOffset(881,-4))
+click(Pattern("1591047774576.png").targetOffset(955,-1))
