4334d29859fbaf9b505d4df686407ce21907e7ba
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-23 12:13:47 +0200

    add missing premium diesel properties to test GasStation in setUp method of GasStationRepositoryTests

--- src/test/java/it/polito/ezgas/repository/GasStationRepositoryTests.java
+++ src/test/java/it/polito/ezgas/repository/GasStationRepositoryTests.java
@@ -143,12 +143,14 @@ public class GasStationRepositoryTests {
 		GS1.setHasMethane(false);
 		GS1.setHasSuper(true);
 		GS1.setHasSuperPlus(false);
+		GS1.setHasPremiumDiesel(true);
 		GS1.setMethanePrice(0.0);
 		GS1.setReportDependability(0);
 		GS1.setReportTimestamp(null);
 		GS1.setReportUser(-1);
 		GS1.setSuperPrice(1.864);
 		GS1.setSuperPlusPrice(0.0);
+		GS1.setPremiumDieselPrice(1.43);
 		GS1.setGasStationId(GS1id);
 		GS1.setLat(45.048903);
 		GS1.setLon(7.659812);
@@ -156,7 +158,7 @@ public class GasStationRepositoryTests {
 		//GS1Dto = GasStationConverter.convertEntityToDto(GS1);
 		gasStationRepository.save(GS1);
 		
-		GS1Dto = new GasStationDto(1, "Esso", "via Olanda, 12, Torino", true, true, false, true, false, "bah", 45.048903, 7.659812, 1.375, 1.864, 0, 1.753, 0, -1, null, 0);
+		GS1Dto = new GasStationDto(1, "Esso", "via Olanda, 12, Torino", true, true, false, true, false, true, "bah", 45.048903, 7.659812, 1.375, 1.864, 0.0, 1.753, 0.0, 1.43, -1, null, 0);
 		GS3 = new GasStation("Repsol", "via Portogallo, 43, Torino", true, true, false, true, false, "IShare", 45.0, 7.0, 1.375, 1.864, 0, 1.753, 0, -1, null, 0);
 		GS3.setGasStationId(3);
 	}
